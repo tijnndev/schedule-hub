9dd1c15aaff859264205705bc79791f383870bd5
Tijnndev27 <t.notkamp@msquad.nl>
2026-06-17 23:24:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const ScheduleHub = require('./scheduleHub');
+const scheduleHub = require('./scheduleHub');
 
-// Export compatibility aliases so consumers can resolve the class in multiple ways.
-module.exports = ScheduleHub;
-module.exports.default = ScheduleHub;
-module.exports.ScheduleHub = ScheduleHub;
-module.exports.Flowcraft = ScheduleHub;
-module.exports.scheduleHub = ScheduleHub;
-module.exports['schedule-hub'] = ScheduleHub;
+module.exports = { scheduleHub };
